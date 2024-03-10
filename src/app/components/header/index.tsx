03eb9804b9735835a1e7c02dafa5394de1fd9514
Nurillo1997 <nurilloabdurazzoqov@gmail>
2024-03-10 09:30:39 +0900

    feat: adding navlink and styling to homepageNavbar

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,7 +1,27 @@
-import { Box, Container, Stack, Typography } from "@mui/material";
+import { Box, Container, Stack } from "@mui/material";
 import React from "react";
+import Avatar from "@mui/material/Avatar";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Tooltip from "@mui/material/Tooltip";
+import PersonAdd from "@mui/icons-material/PersonAdd";
+import Settings from "@mui/icons-material/Settings";
+import Logout from "@mui/icons-material/Logout";
+import { NavLink } from "react-router-dom";
 
 export default function HompePageHeader() {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <div>
       <div>
@@ -18,29 +38,134 @@ export default function HompePageHeader() {
               flexDirection={'row'}
               justifyContent={"space-between"}
             >
-              <Box>Home</Box>
-              <Box>Agents</Box>
-              <Box>Destinations</Box>
-              <Box>Tours</Box>
-              <Box>Orders</Box>
-              <Box>Community</Box>
-              <Box>Help</Box>
+              <Box className="hover_line">
+                <NavLink to="/" activeClassName="underline">
+                  Home
+                </NavLink>
+              </Box>
+              <Box className="hover_line">
+                <NavLink to="/agents-page" activeClassName="underline">
+                Agents
+                </NavLink>
+              </Box>
+              <Box className="hover_line">
+                <NavLink to="/destinations-page" activeClassName="underline">
+                Destinations
+                </NavLink>
+              </Box>
+              <Box className="hover_line">
+                <NavLink to="/tours-page" activeClassName="underline">
+                Tours
+                </NavLink>
+              </Box>
+              <Box className="hover_line">
+                <NavLink to="/orders-page" activeClassName="underline">
+                Orders
+                </NavLink>
+              </Box>
+              <Box className="hover_line">
+                <NavLink to="/community-page" activeClassName="underline">
+                Community
+                </NavLink>
+              </Box>
+              <Box className="hover_line">
+                <NavLink to="/help-page" activeClassName="underline">
+                Help
+                </NavLink>
+              </Box>
             </Stack>
-            <Stack
-              className="topNavRightCont"
-              flexDirection={'row'}
-              justifyContent={"space-between"}
-            >
-              <Box className="topNavRightIcons">
-                <img src="/icons/search.png" alt="search icon" />
-              </Box>
-              <Box className="topNavRightIcons">
-                <img src="/icons/cart.png" alt="" />
-              </Box>
-              <Box className="topNavRightIcons">
-                <img src="/icons/admin.png" alt="" />
-              </Box>
-            </Stack>
+
+            <React.Fragment>
+              <Stack
+                className="topNavRightCont"
+                flexDirection={'row'}
+                justifyContent={"space-between"}
+              >
+                <Box className="topNavRightIcons">
+                  <img src="/icons/search.png" alt="search icon" />
+                </Box>
+                <Box className="topNavRightIcons">
+                  <img src="/icons/cart.png" alt="" />
+                </Box>
+                <Tooltip title="Account settings">
+                  <IconButton
+                  className="topNavRightIcons"
+                    onClick={handleClick}
+                    size="small"
+                    sx={{ ml: 2 }}
+                    aria-controls={open ? "account-menu" : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? "true" : undefined}
+                  >
+                    <Avatar sx={{ width: 50, height: 50, background: '#faf5ee' }}>
+                      <img src="/icons/admin.png" alt="" />
+                    </Avatar>
+                  </IconButton>
+                </Tooltip>
+              </Stack>
+              <Menu
+                anchorEl={anchorEl}
+                id="account-menu"
+                open={open}
+                onClose={handleClose}
+                onClick={handleClose}
+                PaperProps={{
+                  elevation: 0,
+                  sx: {
+                    overflow: "visible",
+                    filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                    mt: 1.5,
+                    "& .MuiAvatar-root": {
+                      width: 32,
+                      height: 32,
+                      ml: -0.5,
+                      mr: 1,
+                    },
+                    "&::before": {
+                      content: '""',
+                      display: "block",
+                      position: "absolute",
+                      top: 0,
+                      right: 14,
+                      width: 10,
+                      height: 10,
+                      bgcolor: "background.paper",
+                      transform: "translateY(-50%) rotate(45deg)",
+                      zIndex: 0,
+                    },
+                  },
+                }}
+                transformOrigin={{ horizontal: "right", vertical: "top" }}
+                anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+              >
+                <MenuItem onClick={handleClose}>
+                  <Avatar /> Profile
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                  <Avatar /> My account
+                </MenuItem>
+                <Divider />
+                <MenuItem onClick={handleClose}>
+                  <ListItemIcon>
+                    <PersonAdd fontSize="small" />
+                  </ListItemIcon>
+                  Add another account
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                  <ListItemIcon>
+                    <Settings fontSize="small" />
+                  </ListItemIcon>
+                  Settings
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                  <ListItemIcon>
+                    <Logout fontSize="small" />
+                  </ListItemIcon>
+                  Logout
+                </MenuItem>
+              </Menu>
+
+            </React.Fragment>
           </Stack>
         </Container>
       </div>
@@ -56,12 +181,12 @@ export default function HompePageHeader() {
             </Box>
 
             <Stack
-              style={{alignItems:'center'}}
+              style={{ alignItems: 'center' }}
               flexDirection={'column'}
               justifyContent={"space-around"}
-              >
+            >
               <Box className="clider_between_text_1">Travel & Adventures</Box>
-              <Box className="clider_between_text_2">travel</Box>
+              <Box className="clider_between_text_2">Where Would You Like To Go?</Box>
 
             </Stack>
 
